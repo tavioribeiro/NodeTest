2b0994fe43a5fed35cb3dafc59089ccc335268ec
Otávio Augusto Ribeiro Araújo <70659535+tavioribeiro@users.noreply.github.com>
2021-08-15 12:35:04 -0300

    Add files via upload

--- n.js
+++ n.js
@@ -66,7 +66,17 @@ app.post("/api/check", (req, res)  =>
     {
       if(result[0].senha === b)
       {
-        res.send(result[0].nome);
+        if(result[0].senha === b)
+          {
+            console.log(result[0]);
+
+            var resultado = 
+            {
+              nome:result[0].nome,
+              id:result[0].id.toString()
+            }
+            res.send(resultado);
+          }
       }
       else
       {
@@ -74,7 +84,153 @@ app.post("/api/check", (req, res)  =>
       }
     }
   })
+})
 
+
+
+app.post("/api/create", (req, res)  =>
+{
+  var a = req.body.a;
+  var b = req.body.b;
+  var c = req.body.c;
+  var d = Math.floor(Math.random() * 999999);
+  
+  console.log("---------------- " + a);
+
+  var stm = "INSERT INTO usuario (login, senha, nome, id) VALUES (?, ?, ?, ?)";
+  db.query(stm, [ a, b, c, d], (err, result) =>
+  {
+    //console.log(result);
+
+    if (err)
+    {
+      res.send('0');
+    } 
+    else if (result.length==0)
+    {
+      res.send('0'); //this is what you are missing
+    }
+    else
+    {
+      var stm2 = "SELECT * FROM usuario where login = ?";
+      db.query(stm2, [ a ], (err, result) =>
+      {
+        if (err)
+        {
+          console.log(err);
+          res.send('0');
+        } 
+        else if (result.length==0)
+        {
+          console.log(err);
+          res.send('0'); //this is what you are missing
+        }
+        else
+        {
+          if(result[0].senha === b)
+          {
+            console.log(result[0]);
+            var resultado = 
+            {
+              a:result[0].nome,
+              b:result[0].id.toString()
+            }
+            res.send(resultado);
+          }
+          else
+          {
+            res.send('1');
+          }
+        }
+      })
+    }
+  })
+})
+
+
+
+
+app.post("/api/postuserdata", (req, res)  =>
+{
+  var a = req.body.a;
+  var b = req.body.b;
+  var c = req.body.c;
+  var d = req.body.d;
+  var e = req.body.e;
+  var f = req.body.f;
+  var g = req.body.g;
+  var h = req.body.h;
+  var i = req.body.i;
+  var j = req.body.j;
+  var k = req.body.k;
+  
+
+
+  console.log("---------------- " + req.body.d );
+
+
+  var stm = "INSERT INTO dadosUsuario (idUsuario, nome, idade, sexo, peso, altura, abdomem, pescoco, quadril, nivelatividade, objetivo) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+  db.query(stm, [ a, b, c, d, e, f, g, h, i, j, k], (err, result) =>
+  {
+    if (err)
+    {
+      console.log(err);
+      res.send('0');
+    } 
+    else if (result.length == 0)
+    {
+      console.log(result[0]);
+      res.send('0'); //this is what you are missing
+    }
+    else
+    {
+      console.log(result[0]);
+      res.send("SUCESSO");
+    }
+  })
+})
+
+
+app.post("/api/updateuserdata", (req, res)  =>
+{
+  var a = req.body.a;
+  var b = req.body.b;
+  var c = req.body.c;
+  var d = req.body.d;
+  var e = req.body.e;
+  var f = req.body.f;
+  var g = req.body.g;
+  var h = req.body.h;
+  var i = req.body.i;
+  var j = req.body.j;
+  var k = req.body.k;
+  
+
+
+  console.log("---------------- " + req.body.d );
+
+  //db.query(stm, [ a, b, c, d, e, f, i, j, k, l, m, n ]
+  //var stm = "INSERT INTO dadosUsuario (id, idUsuario, nome, idade, sexo, peso, altura, abdomem, pescoco, quadril, nivelatividade, objetivo) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+  //db.query(stm, [ a, d, c, c, c, c, c, c, c, c, c, c], (err, result) =>
+  
+  var stm = "UPDATE dadosUsuario SET nome=?, idade=?, sexo=?, peso=?, altura=?, abdomem=?, pescoco=?, quadril=?, nivelatividade=?, objetivo=? WHERE idUsuario=?";
+  db.query(stm, [ b, c, d, e, f, g, h, i, j, k, a], (err, result) =>
+  {
+    if (err)
+    {
+      console.log(err);
+      res.send('0');
+    } 
+    else if (result.length == 0)
+    {
+      res.send('0'); //this is what you are missing
+    }
+    else
+    {
+      console.log(result[0]);
+      res.send("SUCESSO");
+    }
+  })
 })
 
 app.listen(process.env.PORT || port, () =>
@@ -82,4 +238,3 @@ app.listen(process.env.PORT || port, () =>
   //console.clear();
   console.log("================================================================================");
 });
-
